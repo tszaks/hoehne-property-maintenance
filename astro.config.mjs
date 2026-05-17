17fa50d45c3dfb363b39d5263c849316383001dc
Tyler Szakacs <tylerszakacs@gmail.com>
2026-05-17 10:46:05 -0400

    fix: static output, remove node adapter, set correct site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gnaworks.com',
+  site: 'https://www.hoehnepropertymaintenance.com',
   output: 'static',
-  adapter: node({ mode: 'standalone' }),
   integrations: [svelte()],
-
   vite: {
     plugins: [tailwindcss()]
   }
